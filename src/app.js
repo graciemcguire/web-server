3f27972d629a39f9646a5f7f67fa0f123c49dc68
Gracie <m.graciemcguire@gmail.com>
2020-05-11 14:00:06 -0600

    css

--- src/app.js
+++ src/app.js
@@ -1,29 +1,65 @@
 const path = require('path')
 const express = require('express')
-const app = express()
-const publicPath = path.join(__dirname, '../public')
+const hbs = require('hbs')
 
+const app = express()
+
+//expres config
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+// handlebars views set up
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//static directory
 app.use(express.static(publicPath))
 
+
 app.get('', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Weather',
+    name: 'Gracie McGuire'
+  })
 })
 
-//get takes in 2 arguments - route and a function
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Gracie McGuire'
+  })
+})
 
-// app.get('/help', (req, res) => {
-//   res.send('help page')
-// })
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Gracie McGuire',
+    message: 'hi how can i help u'
+  })
+})
 
-// app.get('/about', (req, res) => {
-//   res.send({ "page": 'about', "test": 1 })
-// })
 
 app.get('/weather', (req, res) =>{
   res.send({
     forecast: 'sunny',
     location: 'nyc'
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    message: 'Help article not found.',
+    name: 'Gracie McGuire'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    message: 'This page cannot be found.',
+    name: 'Gracie McGuire'
   })
 })
 
